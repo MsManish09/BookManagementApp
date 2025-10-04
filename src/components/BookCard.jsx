66e7869b43cb2109c8a47f29ef24e6abdf704372
MsManish09 <msmanish680@gmail.com>
2025-10-04 20:39:51 +0530

    search functionality added

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -4,13 +4,13 @@ function BookCard({books}){
     return(
         books.map( (book)=>{
             return(
-                <div className=" bg-[white] border-2 border-solid border-[#F9F5FF] flex flex-col gap-1 w-[275px]  h-[350px] items-center p-1 justify-center shadow-2xl rounded-[10px] hover:scale-95 hover:border-[#D4C2FC] ">
+                <div className=" bg-[white] border-2 border-solid border-[#dadada] flex flex-col gap-1 w-[275px]  h-[350px] items-center p-1 justify-center shadow-2xl rounded-[10px] hover:scale-95 hover:border-[#D4C2FC] ">
                     <img src={book.imageUrl} className=" rounded-2xl h-[200px] w-[150px] " alt="Book image" />
                     <div id="bookDetails" className=" ml-2 flex-col gap-1.5 mt-2 overflow-hidden ">
                         <h1 className=" text-[1.2rem] font-semibold underline " >{book.title}</h1>
-                        <span className="text-[1rem] text-gray-600 " > {book.author} </span>
+                        <span className="text-[1rem] text-gray-600 ml-1 " > {book.author} </span>
                         <span className=" font-semibold "> {book.year} </span>
-                        <p> {book.shortDescription} </p>
+                        <p className=" ml-1 "> {book.shortDescription} </p>
                     </div>
                 </div>
             )
